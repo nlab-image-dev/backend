46e24f0bec2b11e5f74dc050160e117ff2fd5665
Koki Ota <ota-koki-gy@ynu.jp>
2021-09-07 01:20:19 +0900

    change directory

--- api_tester.py
+++ api_tester.py
@@ -25,11 +25,28 @@ def signup():
         "password": "taro",
     }
 
-    requests.post(url, json.dumps(data), headers=headers)
+    response = requests.post(url, json.dumps(data), headers=headers)
 
 def login():
-    pass
+    url = "http://localhost:8000/api/login/"
+    headers = {'Content-Type': 'application/json'}
+    data = {'username': 'Taro', 'password': 'taro'}
+    response = requests.post(url=url, data=json.dumps(data), headers=headers)
+    # print(response, response.text)
+    print(response.json())
+
+    myinfo(response.json()['token'])
+
+def myinfo(token):
+    url = "http://localhost:8000/api/myinfo/"
+    # GET
+    headers = {'Content-Type': 'application/json', 'Authorization': f'JWT {token}'}
+    response = requests.get(url=url, headers=headers)
+    print(response.json())
+
+
 
 if __name__ == "__main__":
-    article_post()
+    # article_post()
     # signup()
+    login()
