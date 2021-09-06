20045db7457f57056bce34b02e9b1defb8ebcbd6
Koki Ota <ota-koki-gy@ynu.jp>
2021-09-06 21:59:01 +0900

    fixed model

--- main/api/models.py
+++ main/api/models.py
@@ -11,10 +11,11 @@ class ArticleModel(models.Model):
     text = models.TextField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     posted_time = models.DateTimeField()
-    file = models.FileField(null=True)
+    file = models.FileField(upload_to="media/%Y%m%d/", null=True)
 
 class CommentModel(models.Model):
     text = models.TextField()
+    article_id = models.ForeignKey(ArticleModel, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     posted_time = models.DateTimeField()
     
